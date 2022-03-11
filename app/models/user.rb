6383e1c1c28ec4316ae1c25bd5dc6b6f6585c671
shouko.yamashita <white_lemontea@yahoo.co.jp>
2022-03-11 12:05:59 +0000

    devise用にUserモデルの変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   before_save { self.email.downcase! }
   validates :name, presence: true, length: { maximum: 50 }
   validates :prof, length: { maximum: 500 }
