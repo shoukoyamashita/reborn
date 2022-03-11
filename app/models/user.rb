d2fd92cd0267e1746a9e0f9555e1be963499e844
shouko.yamashita <white_lemontea@yahoo.co.jp>
2022-03-11 13:56:42 +0000

    Userモデルに:confirmable を追記

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :validatable
   before_save { self.email.downcase! }
   validates :name, presence: true, length: { maximum: 50 }
