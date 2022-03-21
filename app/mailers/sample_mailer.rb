891572f9576820170e6f198833ef26fb84054eed
shouko.yamashita <white_lemontea@yahoo.co.jp>
2022-03-21 04:08:43 +0000

    コメント登録時のメール通知ができていなかったのでできるように修正

--- app/mailers/sample_mailer.rb
+++ app/mailers/sample_mailer.rb
@@ -3,7 +3,7 @@ class SampleMailer < ApplicationMailer
   def get_comment(user)
     @user = user
     
-    @comment_user = @comment.user
+    # @comment_user = @comment.user
 
     mail to:      @user.email,
         subject: '投稿にコメントがつきました。'
